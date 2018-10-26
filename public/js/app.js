a02cedbbc0cd6489987735e8dd09840262661320
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-10-26 14:02:14 -0500

    made connection to database

---
+++ public/js/app.js
@@ -0,0 +1,45 @@
+
+$(function() {
+
+
+    //function to make an ajax call back to api to get the products
+    const runProductQuery = function () {
+
+     $.ajax({ url: '/api/products', method: 'GET'}).then(function(productList) {
+
+        renderProducts('#productList', productList);
+
+     })   
+
+    }
+
+    //function to render/print the data from the database to our html page
+    const renderProducts = function(outputElement, dataList)
+    {
+        for(let i = 0; i < dataList.length; i++) {
+
+            //getting a reference to the productList and populating it with tables
+            const output = $(outputElement);
+        
+        //displaying it in the HTML sections
+        const listItem = $('<li class=\'list-group-item mt-4\'>');
+
+        listItem.append(
+            $('<h3>').text('Product #' + (i + 1)),
+            $('<hr>'),
+            $('<h2>').text('Name: ' +  dataList[i].product_name),
+            $('<h2>').text('Department Name: ' +  dataList[i].department_name),
+            $('<h2>').text('Price: $' +  dataList[i].price),
+            $('<h2>').text('Quantity: ' +  dataList[i].stock_quantity)
+        );
+
+        output.append(listItem);
+
+        }
+    }
+
+
+    runProductQuery();
+
+
+})
