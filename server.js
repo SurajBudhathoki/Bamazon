1e6844523db1df18a2aad1009fa0169e23347e59
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-10-30 11:24:49 -0500

    herokuuu

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.JAWSDB_URL || 8080;
+const PORT = process.env.JAWSDB_URL || 3000;
 
 
 app.use(express.urlencoded({ extended: true}));
